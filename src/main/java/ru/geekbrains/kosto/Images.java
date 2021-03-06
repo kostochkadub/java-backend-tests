ec6cfdcc8457d8788cda5aeb18abd96952913692
Konstantin <kostochkadub@gmail.com>
2021-03-06 11:12:29 +0300

    fix resources

--- src/main/java/ru/geekbrains/kosto/Images.java
+++ src/main/java/ru/geekbrains/kosto/Images.java
@@ -1,11 +1,11 @@
 package ru.geekbrains.kosto;
 
 public enum Images {
-    IMAGE_10MB("src/test/java/ru/geekbrains/kosto/img/repository/10mb.png"),
-    IMAGE_MORE_10MB("src/test/java/ru/geekbrains/kosto/img/repository/more_10.png"),
-    IMAGE_SMALL_SIZE("src/test/java/ru/geekbrains/kosto/img/repository/less_1_kb.png"),
-    IMAGE_LESS_10MB("src/test/java/ru/geekbrains/kosto/img/repository/less_10.png"),
-    TEXT_FILE("src/test/java/ru/geekbrains/kosto/img/repository/test.txt");
+    IMAGE_10MB("src/test/resources/repository/10mb.png"),
+    IMAGE_MORE_10MB("src/test/resources/repository/more_10.png"),
+    IMAGE_SMALL_SIZE("src/test/resources/repository/less_1_kb.png"),
+    IMAGE_LESS_10MB("src/test/resources/repository/less_10.png"),
+    TEXT_FILE("src/test/resources/repository/test.txt");
 
     public final String path;
 
